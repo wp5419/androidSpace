332cb7a6430ceb2729096325934a064ba2252c2b
wp5419 <329713452@qq.com>
2016-09-22 18:04:00 +0800

    1
    1

--- MyApplication2/app/src/main/java/com/example/wafer/myapplication/Main.java
+++ MyApplication2/app/src/main/java/com/example/wafer/myapplication/Main.java
@@ -25,13 +25,11 @@ public class Main extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout);
         x.Ext.init(getApplication());
-        Button btn = (Button) findViewById(R.id.login);
-        Button pBtn = (Button) findViewById(R.id.cancle);
-        Button cBtn = (Button) findViewById(R.id.clear);
-
     }
     public void onWeatherClick(View view){
-        RequestParams rp = new RequestParams(GlobalParam.TEST_URL);
+        String url = "http://192.168.0.154/messageCenter/oa/doc/findFileList?token=DBC04F578D15C8DD7FC200F6E2E7040722B29B2A5F2BC70A9C9E7B3289D2618CB16FE7824FB431FC021355CD7804C373AE16D52E5853910AA80E162DDE9646B3&id=1";
+
+        RequestParams rp = new RequestParams(url);
         x.http().get(rp, new Callback.CommonCallback<String>() {
             @Override
             public void onSuccess(String result) {
